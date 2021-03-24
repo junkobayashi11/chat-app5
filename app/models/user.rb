5d18b1be3169b4ca6d23f7b2c94acddb31414448
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-03-24 19:46:21 +0900

    feat: room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  has_many :rooms, throught: :room_users
+  has_many :room_users
 end
